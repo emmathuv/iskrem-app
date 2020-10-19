e3ff8d374e8880abaa448a78ccdfe95d981e4265
emmathuv <72164952+emmathuv@users.noreply.github.com>
2020-10-19 17:42:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Norsk Kalender</title>
+        <link rel="stylesheet" href="main.css"/>
+    </head>
+
+    <body>
+        <div id = "container">
+            <div id = "header">
+                <h1>Norsk Kalender 2021</h1>
+            </div>
+            <div id = "content">
+                <div id = "nav">
+                    <h3>Naviger</h3>
+                    <ul>
+                        <li><a class = "selected" href = "">Hjem</a></li>
+                        <li><a href="page2.html">Påske</a></li>
+                        <li><a href="page3.html">Pinse</a></li>
+                        <li><a href="page4.html">Helligdager</a></li>
+                    </ul>
+                </div>
+
+                <div id = "main">
+                    <h2>Kalender med helligdager</h2>
+
+                    <p>Lurer du på hvilken uke det er?</p>
+                    
+                    <p>Når er påsken 2021? Hvilken dag faller 1. og 17. mai på?</p>
+                    
+                    <p>Hvilke dager er det fri i julen?</p>
+                    
+                    <p>Alt dette finner du lett ut av på Norsk Kalender.</p>
+
+                </div>
+                <div id = "Calendar">
+                    <iframe width="1100" height="800" frameborder="0" scrolling="no" src="https://onedrive.live.com/embed?resid=2EEF3BD76E3346A2%213495&authkey=%21AJQEqOSzBI2LMJM&em=2&wdAllowInteractivity=False&Item='Sheet1'!B2%3AAA36&wdHideGridlines=True&wdDownloadButton=True&wdInConfigurator=True"></iframe>
+                </div>
+
+
+                </div>
+            </div>
+            <div id = "footer">
+                Copyright &copy; 2020 Ems&Marra 
+
+            </div>
+        </div>
+
+    </body>
+
+</html>
